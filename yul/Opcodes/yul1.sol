1152a93c7a0ce8bdc2d6acfd52d3ccfbdf88025c
veridelisi <72710290+veridelisi@users.noreply.github.com>
2023-04-07 15:54:50 +0300

    Create yul1.sol

---
+++ yul/Opcodes/yul1.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+// compiler version must be greater than or equal to 0.8.17 and less than 0.9.0
+// https://github.com/Perelyn-sama/yul_by_example/blob/main/src/HelloWorld.sol
+
+pragma solidity 0.8.19;
+
+
+contract HelloWorld {
+    function Greet() external pure returns (string memory) {
+        assembly {
+            // Assign the string to var `greet`
+            // "Hello World!" => 0x48656c6c6f20576f726c64210000000000000000000000000000000000000000.
+            let greet := 0x48656c6c6f20576f726c64210000000000000000000000000000000000000000
+            // Store the string offset in mem[0x00].
+            // This is an ABI requirement, 0x20 must be stored at any chosen offset.
+            mstore(0x00, 0x20)
+            // Store the length of the string in mem[offset + 32 bytes].
+            mstore(0x20, 0x0c) // 0x0c = 12, length of "Hello World!".
+            // Store the string in mem[offset + 64 bytes].
+            mstore(0x40, greet)
+            // Returns the bytes from mem[offset to offset+size]
+            return(0x00, 0x60)
+        }
+    }
+}
